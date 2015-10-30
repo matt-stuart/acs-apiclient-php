d6210edda45d84c497881d078b4e63683cfdeb49
Alexei White <alexei.white@gmail.com>
2015-10-30 14:37:05 -0700

    1.0

---
+++ AcsClient.php
@@ -0,0 +1,527 @@
+<?php
+
+class AcsClient
+{
+    const SIGNATURE_METHOD_HMACSHA1 = "HMAC-SHA1";
+
+    public function __construct(array $config = array())
+    {        
+        $this->setOptions($config);
+    }
+
+    /**
+     * Sets internal options from provided array. Will use individual setters if option key matches method name
+     *
+     * @param array $options
+     * @return $this
+     */
+    public function setOptions(array $options)
+    {
+        foreach ($options as $key => $value) {
+            $method = "set" . ucfirst($key);
+            if (method_exists($this, $method)) {
+                $this->{$method}($value);
+            } else {
+                $this->_options[$key] = $value;
+            }
+        }
+        return $this;
+    }
+
+    /**
+     * Magic function implements proxy "get*" functions
+     *
+     * @param string $name
+     * @param array $arguments
+     * @return mixed
+     * @throws InvalidArgumentException
+     */
+    public function __call($name, $arguments)
+    {
+        if (substr($name, 0, 3) === "get") {
+            $attribute = "_" . ($property = lcfirst(substr($name, 3)));
+            if (property_exists($this, $attribute)) {
+                return $this->{$attribute};
+            }
+            if (isset($this->_options[$property])) {
+                return $this->_options[$property];
+            }
+        }
+        throw new InvalidArgumentException("Method not defined '$name'");
+    }
+
+    public function callResource($resource, $method = null, $data = null, $callback = null)
+    {
+        if (!is_string($resource) || !$resource) {
+            throw new \InvalidArgumentException("Unexpeted \$resource provided");
+        }
+
+        $defaultMethod = "GET";
+        if (is_callable($method)) {
+            $callback = $method;
+            $data = array();
+            $method = $defaultMethod;
+        }
+        if (is_callable($data)) {
+            $callback = $data;
+            $data = array();
+        }
+
+        $method = strtoupper(trim($method)) ?: $defaultMethod;
+        if (!in_array($method, array("GET", "POST", "DELETE", "PUT"))) {
+            throw new InvalidArgumentException("Invalid supplied method '{$method}'");
+        }
+
+        $data = $data ?: array();
+        if (!is_array($data)) {
+            throw new InvalidArgumentException("Expected array for extra data");
+        }
+        if (!$callback || !is_callable($callback)) {
+            $callback = function() { /* no op */ };
+        }
+
+        /* Clean the path */
+        $resource = str_replace("\\", "/", $resource);
+        if ($resource[0] == "/") {
+            $resource = substr($resource, 1);
+        }
+
+        if (!$this->_establishAuthorization()) {
+            $callback($this);
+            return false;
+        }
+
+        $uri = new \OAuth\Common\Http\Uri\Uri($this->getOption('serviceRoot') . $resource);
+
+        $oa = $this->_getOAuth();
+        $response = $oa->request(
+            $resource,
+            $method,
+            null,
+            array()
+        );
+
+        $info = $oa->getLastRequest();
+        if (!(isset($info['info']) && isset($info['info']['http_code']) && $info['info']['http_code'] == 200)) {
+            // Error State
+            $http_code = 0;
+            if (isset($info['info']) && isset($info['info']['http_code'])) {
+                $http_code = $info['info']['http_code'];
+            }
+
+            switch ($http_code) {
+                case 404:
+                    $this->setError("404", "Unknown resource '$resource'");
+                    break;
+                case 403:
+                    $this->setError("403", "Access denied to '$resource'");
+                    break;
+                default:
+                    $this->setError("INVALIDREQUEST", "Request to '$resource' could not complete. Please try again.");
+                    break;
+            }
+        } else if (!is_string($response) || !$response = json_decode($response, true)) {
+            $this->setError("INVALIDREQUEST", "Request to '$resource' could not complete. Please try again.");
+        }
+
+        if ($this->isError()) {
+            $callback($this);
+            return false;
+        }
+
+        $this->setError(false);
+        $this->setLastResponse($response);
+        $callback($this);
+
+        return $this;
+    }
+
+    public function getDateObject($clientId, $date)
+    {
+        $arguments = func_get_args();
+
+        // Remove the client id
+        array_shift($arguments);
+        if (!count($arguments) || !$from = array_shift($arguments)) {
+            throw new \InvalidArgumentException("Invalid arguments, date creation requires at least one date parameter");
+        }
+
+        $to = $style = $fiscal = $option = null;
+        if ($arguments) {
+            if (is_object($fiscal = end($arguments))) {
+                $fiscal = null;
+            }
+            if (count($arguments) > 4) {
+                $arguments = array_slice($arguments, 0, 4);
+            }
+            switch (count($arguments)) {
+                case 4:
+                    list($to, $style, $option, $fiscal) = $arguments;
+                    break;
+                case 3:
+                    list($to, $style, $option) = $arguments;
+                    break;
+
+                case 2:
+                    list($to, $style) = $arguments;
+                    $option = $style;
+                    break;
+                case 1:
+                    $to = $arguments[0];
+                    $option = $style = $to;
+                    break;
+            }
+        }
+
+        if (!class_exists('ForeseeDate', false)) {
+            require_once __DIR__ . '/vendor/ForeseeDate.php';
+        }
+        return \ForeseeDate::create($clientId, $from, $to, $style, $fiscal, $option);
+    }
+
+    public function setUsername($username)
+    {
+        $this->_username = $username;
+        return $this;
+    }
+
+    public function setPassword($password)
+    {
+        $this->_password = $password;
+        return $this;
+    }
+
+    public function setConsumerKey($key)
+    {
+        $this->_consumerKey = $key;
+        return $this;
+    }
+
+    public function setConsumerSecret($secret)
+    {
+        $this->_consumerSecret = $secret;
+        return $this;
+    }
+
+    public function setLastResponse($response)
+    {
+        $this->_lastResponse = $response;
+    }
+
+    public function getLastResponse()
+    {
+        return $this->_lastResponse;
+    }
+
+
+    public function setError($code, $message = "")
+    {
+        if ($code) {
+            $code = array(
+                'code' => $code,
+                'message' => $message
+            );
+        }
+        $this->_error = $code;
+        return $this;
+    }
+
+    public function isError()
+    {
+        return !!$this->_error;
+    }
+
+    public function getError()
+    {
+        return $this->_error;
+    }
+
+    public function getSignatureMethod()
+    {
+        return $this->_signatureMethod ?: static::SIGNATURE_METHOD_HMACSHA1;
+    }
+
+    public function getOption($option)
+    {
+        if (isset($this->_options[$option])) {
+            return $this->_options[$option];
+        }
+
+        return null;
+    }
+
+    public function setAccessToken($token)
+    {
+        if (is_string($token)) {
+            $token = array('accessToken' => $token);
+            if (is_array($this->_accessToken)) {
+                $token = array_merge($this->_accessToken,array('accessToken' => $token));
+            }
+        }
+        if (!is_array($token)) {
+            throw new \InvalidArgumentException("Invalid access token provided. Expect string or array");
+        }
+
+        $this->_accessToken = $token;
+        return $this;
+    }
+
+    public function setAccessTokenSecret($token)
+    {
+        if (!is_string($token)) {
+            throw new \InvalidArgumentException("Invalid access token secret provided. Expected string");
+        }
+
+        if (!is_array($this->_accessToken)) {
+            $this->_accessToken = array();
+        }
+        $this->_accessToken['accessTokenSecret'] = $token;
+        return $this;
+    }
+
+    public function getAccessToken()
+    {
+        return $this->_accessToken;
+    }
+
+    public function authenticate()
+    {
+        $this->_oa = null;
+        $this->_oaData = array();
+        $this->_accessToken = null;
+
+        return $this->_establishAuthorization();
+    }
+
+
+    protected $_consumerKey;
+    protected $_consumerSecret;
+    protected $_accessToken;
+    protected $_signatureMethod;
+
+    protected $_username;
+    protected $_password;
+
+
+    protected $_lastResponse = null;
+    protected $_error = false;
+
+    /**
+     * Options array
+     * @var array
+     */
+    protected $_options = array(
+        'serviceRoot' => 'https://portal2.foreseeresults.com/services/',
+        'requestTokenUri' => 'oauth/request_token',
+        'accessTokenUri' => 'oauth/access_token',
+        'authorizationUri' => 'oauth/user_authorization',
+        'loginUri' => 'login',
+        'sessionId' => null
+    );
+
+    /**
+     *
+     * @var \OAuth\OAuth1\Service\Foresee
+     */
+    protected $_oa = null;
+
+    protected $_oaData = array();
+    
+    protected function _getOAuth()
+    {
+        if (!$this->_oa) {
+            require_once __DIR__  . '/vendor/OAuth/bootstrap.php';
+
+            $credentials = new \OAuth\Common\Consumer\Credentials(
+                $this->getConsumerKey(),
+                $this->getConsumerSecret(),
+                'oob'
+            );
+
+            $cookieJar = tempnam(sys_get_temp_dir(), "acs_");
+            $this->setOptions(array('cookieJar' => $cookieJar));
+
+            $client = new \OAuth\Common\Http\Client\CurlClient('ACSOAuthLibrary');
+            $client->setMaxRedirects(0);
+            $client->setCurlParameters(array(
+                CURLOPT_COOKIEFILE => $cookieJar,
+                CURLOPT_COOKIEJAR => $cookieJar
+            ));
+
+            $storage = new \OAuth\Common\Storage\Memory();
+
+            $signature = new \OAuth\OAuth1\Signature\Signature($credentials);
+            $signature->setHashingAlgorithm($this->getSignatureMethod());
+
+            $baseUri = new \OAuth\Common\Http\Uri\Uri($this->getOption('serviceRoot'));
+
+            $this->_oa = new \OAuth\OAuth1\Service\Foresee(
+                $credentials,
+                $client,
+                $storage,
+                $signature,
+                $baseUri
+            );
+
+            if ($this->_accessToken) {
+                $token = $this->_oa->createAccessToken(array(
+                    'oauth_token' => $this->_accessToken['accessToken'],
+                    'oauth_token_secret' => $this->_accessToken['accessTokenSecret']
+                ));
+                $this->_oa->setAccessToken($token);
+                $this->_oaData['accessToken'] = $token;
+            }
+        }
+
+        return $this->_oa;
+    }
+
+    protected function _finishOAuth()
+    {
+        if ($this->_oa) {
+            if ($cj = $this->getOption('cookieJar')) {
+                @unlink($cj);
+            }
+        }
+    }
+
+    protected function _establishAuthorization()
+    {
+        $oa = $this->_getOAuth();
+
+        if (isset($this->_oaData['accessToken'])) {
+            return true;
+        }
+
+        try {
+            /**
+             * *** REQUEST TOKEN
+             */
+            $this->_oaData['requestToken'] = $requestToken = $oa->requestRequestToken();
+
+            if (!$requestToken) {
+                // Add error return false
+                $this->setError("INVALIDREQUESTTOKEN", "Unable to retrieve request token from service.");
+                return false;
+            }
+
+            /**
+             * @var \OAuth\Common\Http\Client\CurlClient $request
+             */
+            $request = $oa->getHttpClient();
+
+            /**
+             * LOGIN
+             */
+            $response = $request->retrieveResponse(
+                $oa->getLoginEndpoint(),
+                array(
+                    'j_username' => $this->getUsername(),
+                    'j_password' => $this->getPassword()
+                ),
+                array(),
+                "POST"
+            );
+
+            $info = $request->getLastRequest('info');
+            if ($info['http_code'] != 302) {
+                $this->setError("COULDNOTLOGIN", "Unexpected response from the OAuth service, unable to authorize.");
+                return false;
+            }
+
+            $redirectUrl = $info['redirect_url'];
+            if (strpos($redirectUrl, '#loginfailed') !== FALSE) {
+                $this->setError("INVALIDCREDENTIALS", "Unable to login, username and password are invalid.");
+                return false;
+            }
+
+            /**
+             * CHECK AUTHORIZATION / GET VERIFIER
+             */
+            $response = $request->retrieveResponse(
+                $this->_oa->getAuthorizationEndpoint(array(
+                    'oauth_token' => $requestToken->getRequestToken()
+                )),
+                null,
+                array(),
+                "GET"
+            );
+            $last = $request->getLastRequest();
+            $info = $last['info'];
+            if ($info['http_code'] != '302') {
+                $this->setError("COULDNOTGETACCESSTOKEN", "Unexpected response from the OAuth service, unable to authorize.");
+                return false;
+            }
+
+            $url = $info['redirect_url'];
+            if (strpos($url, '?') === FALSE) {
+                $this->setError("COULDNOTGETACCESSTOKEN", "Unexpected response from the OAuth service, unable to authorize.");
+                return false;
+            }
+
+            list($host, $url) = explode('?', $url, 2);
+            parse_str($url, $values);
+            if (!isset($values['oauth_verifier']) || strlen($values['oauth_verifier']) < 2) {
+                $this->setError("COULDNOTFINDVERIFIER", "Unexpected response from OAuth service, unable to complete authorization.");
+                return false;
+            }
+
+            $this->_oaData['oauth_verifier'] = $values['oauth_verifier'];
+
+            /**
+             * ACCESS TOKEN
+             */
+            $oa->setHttpClient($request);
+            $accessToken = $oa->requestAccessToken(
+                $requestToken->getRequestToken(),
+                $this->_oaData['oauth_verifier'],
+                $requestToken->getRequestTokenSecret()
+            );
+
+            if (!$accessToken) {
+                $this->setError("COULDNOTGETACCESSTOKENNULL", "Unable to retrieve access token, please try again.");
+                return false;
+
+            }
+            $this->setAccessToken($accessToken->getAccessToken())->setAccessTokenSecret($accessToken->getAccessTokenSecret());
+            $this->_oaData['accessToken'] = $accessToken;
+            return true;
+        } catch (Exception $e) {
+            // Add error
+            $this->setError("COULDNOTLOGIN", "Error while authorizing. " . $e->getMessage());
+            return false;
+        }
+    }
+
+    protected function _generateResourceUri($resource, $params = array())
+    {
+        $url = $this->getOption('serviceRoot') . $resource;
+        if ($params) {
+            if (strpos($url, "?") === FALSE) {
+                $url .= "?";
+            }
+            $url .= http_build_query($params);
+        }
+        return $url;
+    }
+
+    protected function _extractCookies($headerString)
+    {
+        $headerString = str_replace("\r\n", "\n", $headerString);
+        $lines = explode("\n", $headerString);
+
+        $cookies = array();
+        foreach ($lines as $line) {
+            $pieces = explode(":", $line, 2);
+            if (count($pieces) == 2) {
+                if (strtolower($pieces[0]) == "set-cookie") {
+                    $pieces = explode(";", $pieces[1]);
+                    $cookie = trim($pieces[0]);
+                    $cookie = explode('=', $cookie);
+                    $cookies[strtolower($cookie[0])] = $cookie[1];
+                }
+            }
+        }
+        return $cookies;
+    }
+}
--- README.md
+++ README.md
@@ -1,11 +1,213 @@
-Answers Cloud Services API Client Library for PHP
+Answers Cloud Services API Client Library - PHP
 ===================
-Helper library for connecting to the Answers Cloud Services (ForeSee in particular) web API in a headless manner from PHP. You can use this to simplify connecting to the ACS api without requiring a browser or user interaction to grant access to a particular account.
-###Installation
+Helper library for connecting to the Answers Cloud Services (ForeSee in particular) web API in a headless manner. You can use this to simplify connecting to the ACS api without requiring a browser or user interaction to grant access to a particular account.
+###Version Support
+Supports PHP v5.2 and above 
+
+###Installation 
++ Download client library
++ Install extracted library to a sub-path of your codebase
+
 ###Simple Usage
+```php
+require_once realpath('path/to/library/AcsClient.php');
+
+$opts = array(
+  consumerKey: "[CONSUMER KEY PROVIDED BY FORESEE]",
+  consumerSecret: "[CONSUMER SECRET PROVIDED BY FORESEE]",
+  consumerType: "[CONSUMER TYPE PROVIDED BY FORESEE]",
+  username: "[USERNAME]",
+  password: "[PASSWORD]"
+);
+
+/**
+ * Set up the client
+ */
+$client = new AcsClient($opts);
+
+// Call resource endpoint
+$client->callResource(
+    "currentUser", // Resource endpoint 
+    "GET", // HTTP method
+    [], // Extra Parameters necessary for some end points 
+    function($result) { // callback function to be executed on error and success
+        if ($result->isError()) {
+            // Preform actions when errors occur
+        } else {
+            // Preform actions for a successful execution
+            $last = $result->getLastResponse();
+        }
+    }
+);
+```
+Note: Native anonymous function implementation above requires PHP5.3, see http://php.net/manual/en/functions.anonymous.php
+
 ###Errors
+The ACS Client class throws InvalidArgumentException, see http://php.net/manual/en/class.invalidargumentexception.php, when creating instances or executing methods with missing or invalid argumnets.
+
+Otherwise, ACS Client returns a FALSE when errors occur after setting internal error variables. If not monitoring the return result, errors can be checked by executing isError() and retrieving the errors by calling getErrors().  
+
+If there is a problem authenticating or reaching an endpoint, or if a request is malformed, an error will be generated and stored internally within the ACS Client instance. Possible error codes:
+* `INVALIDREQUESTTOKEN` - Could not get a request token. There may be something wrong with your consumer key or consumer secret.
+* `COULDNOTLOGIN` - There was a problem with the login process. Probably not due to invalid credentials.
+* `INVALIDCREDENTIALS` - Could not log in with the provided credentials.
+* `COULDNOTAUTHTOKEN` - Could not authorize the auth token.
+* `COULDNOTFINDVERIFIER` - There was a problem with the authentication flow. Might be due to an invalid `consumer_type`, `consumer_key` or `consumer_secret`.
+* `COULDNOTGETACCESSTOKEN` - There was a problem with the authentication flow. Might be due to an invalid `consumer_type`, `consumer_key` or `consumer_secret`.
+* `COULDNOTGETACCESSTOKENNULL` - There was a problem with the authentication flow. Might be due to an invalid `consumer_type`, `consumer_key` or `consumer_secret`.
+* `INVALIDREQUEST` - The request failed for an unknown or generic reason. Please try again.
+* `403` - You do not have access to that endpoint with that criteria.
+* `404` - The endpoint requested could not be found.
+
+Errors are provided as a simple array with keys 'code' and 'msg'. Example:
+```php
+[
+  "code": "COULDNOTGETACCESSTOKENNULL",
+  "msg": "Error getting the access token since they were null."
+]
+```
 ###Authentication
+Authentication will occur automatically, when necessary, as you make service calls. Therefor, if you have not authenticated already and not yet been granted an access token nor an access token secret, the first call to an endpoint will take longer than the rest since it has to go through the authentication flow first.
+
+If you want to check your credentials or just ensure you're actually able to contact an endpoint using the information provided, you can call the `authenticate()` method:
+ 
+```php
+require_once realpath('path/to/library/AcsClient.php');
+
+$opts = array(
+  consumerKey: "[CONSUMER KEY PROVIDED BY FORESEE]",
+  consumerSecret: "[CONSUMER SECRET PROVIDED BY FORESEE]",
+  consumerType: "[CONSUMER TYPE PROVIDED BY FORESEE]",
+  username: "[USERNAME]",
+  password: "[PASSWORD]"
+);
+
+/**
+ * Set up the client
+ */
+$client = new AcsClient($opts);
+if ($client->authenticate()) {
+    // Authenticated
+} else {
+    // Error state
+}
+```
+
 ###Accessing without Credentials
+ACS uses an oAuth authentication scheme so you may want to take advantage of the fact that once you have an access token and an access token secret, you should not need to keep user credentials around to continue using the service layer. ACS can issue you a long-lived token if you request it so you can just set the following attributes on the `options` object rather than the u/p combo:
+```php
+// Authenticating without usernames or passwords
+require_once realpath('path/to/library/AcsClient.php');
+
+$opts = array(
+  consumerKey: "[CONSUMER KEY PROVIDED BY FORESEE]",
+  consumerSecret: "[CONSUMER SECRET PROVIDED BY FORESEE]",
+  consumerType: "[CONSUMER TYPE PROVIDED BY FORESEE]",
+  accessToken: "[AUTHENTICATED ACCESS TOKEN]",
+  accessTokenSecret: "[AUTHENTICATED ACCESS TOKEN SECRET]"
+);
+
+/**
+ * Set up the client
+ */
+$client = new AcsClient($opts);
+
+$client->callResource(
+    "currentUser", // Resource endpoint 
+    "GET", // HTTP method
+    [], // Extra Parameters necessary for some end points 
+    function($result) { // callback function to be executed on error and success
+        if ($result->isError()) {
+            // Preform actions when errors occur
+        } else {
+            // Preform actions for a successful execution
+            $last = $result->getLastResponse();
+        }
+    }
+);
+```
+Using this technique can result in performance improvements since it eliminates the need to move through the authentication flow. The only downside is, if your token has expired, then you will need to authenticate again with your username and password.
+
+Getting your access token and secret is easy: just look for them on the `client.opts` object:
+```php
+/**
+ * @var array $accessToken ['accessToken' => '..', 'accessTokenSecret' => '..']
+ */
+$accessToken = $client->getAccessToken();
+print_r($accessToken);
+```
 ###Date Criteria
+Many of the endpoints accept dates or date ranges as filtering criteria. The full documentaton for how to format these can be found at the ACS developer portal (http://bit.ly/15uYi0k). We've provided a convenience method to help generate these terse date objects:
+```php
+$clientID = "[ASSIGNED CLIENTID]";
+
+$client->callResource(
+    "endPointUri", // Resource endpoint 
+    "GET", // HTTP method
+    [
+        "criteria" => [
+            "dateRange" => $client->getDateObject($clientId, "WEEKTODATE")
+        ]
+    ],  
+    function($result) { // callback function to be executed on error and success
+        if ($result->isError()) {
+            // Preform actions when errors occur
+        } else {
+            // Preform actions for a successful execution
+            $last = $result->getLastResponse();
+        }
+    }
+);
+```
+The first argument to `getDateObject()` is always the client id (Number). This is used to handle some custom calendaring functions like fiscal calendar. The rest of the arguments depend on what you want. The second argument can be one of the following constants, for example:
+* `WEEKTODATE` - The current week of data.
+* `LASTWEEK` - Last week's data.
+* `YESTERDAY` - Yesterday's data. Note there is no "today" currently.
+* `MONTHTODATE` - The current month of data.
+* `LASTMONTH` - Last month
+* `QUARTERTODATE` - The current fiscal quarter of data
+* `LASTQUARTER` - The last fiscal quarter of data
+* `YEARTODATE` - The current year of data.
+* `LASTYEAR` - Last year
+
+You can also construct more complex ranges that involve several arguments. Here are come examples to get you started:
+```php
+$client->getDateObject($clientId, "LAST", 3, "DAYS");
+$client->getDateObject($clientId, "LAST", 6, "MONTHS");
+$client->getDateObject($clientId, "LAST", 2, "YEARS");
+```
+Of course you can also get date objects for specific dates and date ranges:
+```php
+$client->getDateObject($clientId, "2013-03-04");  // Get for April 4, 2013 by string
+$client->getDateObject($clientId, new \DateTime("2013-03-04");  // Get for April 4, 2013 by object
+
+// Get for a range between April 4, 2013 and January 1, 2014:
+$client->getDateObject($clientId, "2014-03-04", "2015-01-01");  
+```
 ###Fiscal Calendars
+Normally, relative and general dates are calculated using normal calendar (Gregorian) dates. If your account is set up with fiscal calendars, you can switch to this instead by passing `FISCAL` as the last argument:
+```php
+$client->getDateObject($clientId, "LAST", 2, "YEARS", "FISCAL");
+```
 ###Date Comparisons
+To perform a date comparison on a period period, which is possible on some endpoints, make the last argument in your `getDateObject()` call equal to `PRIORPERIOD`. Then, assign this to the `dateRangeCompare` attribute of the criteria object:
+```php
+$client->callResource(
+    "endPointUri", // Resource endpoint 
+    "GET", // HTTP method
+    [
+        "criteria" => [
+            "dateRange" => $client->getDateObject($clientId, "WEEKTODATE"),
+            "dateRangeCompare" => $client->getDateObject($clientId, "WEEKTODATE", "PRIORPERIOD")
+        ]
+    ],  
+    function($result) { // callback function to be executed on error and success
+        if ($result->isError()) {
+            // Preform actions when errors occur
+        } else {
+            // Preform actions for a successful execution
+            $last = $result->getLastResponse();
+        }
+    }
+);
+```
